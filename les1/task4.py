cde8ad81dcd1dd4954967ae281aae675917b49bc
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-06-14 22:52:42 +0300

    fin

--- les1/task4.py
+++ les1/task4.py
@@ -7,3 +7,20 @@
 Например, если надо получить случайный символ от 'a' до 'f',то вводятся эти символы.
 Программа должна вывести на экран любой символ алфавита от 'a' до 'f' включительно.
 """
+import time
+
+#print((round(time.time()*1000%15000,2)))
+b, e = input('введите диапазон чисел, сиволов или вещественных чисел через пробел: ').split()
+
+if b.isalpha() and e.isalpha():
+    len_ = abs(ord(b) - ord(e)) + 1
+    print(f'Случайный символ из заданного диапазона - {chr(min(ord(b),ord(e)) + int(time.time()*1000%len_))}')
+elif ((b.isdigit() or (b[0] == '-' and b[1:].isdigit))
+      and (e.isdigit() or (e[0] == '-' and e[1:].isdigit))):
+    b, e = map(int, (b, e))
+    len_ = abs(b - e) + 1
+    print(f'Случайное число из заданного диапазона - {int(time.time()*1000%len_)}')
+elif b.find('.') and e.find('.'):
+    b, e = map(float, (b, e))
+    len_ = abs(b - e) + 1
+    print(f'Случайный символ из заданного диапазона - {round(time.time()*1000%len_,5)}')
--- les1/task5.py
+++ les1/task5.py
@@ -3,3 +3,11 @@
 """
 
 ALPHABET = 'abcdefghiklmnopqrstvxyz'
+
+a, b = input('введите две буквы через пробел : ').split()
+
+index_a = ALPHABET.find(a) + 1
+index_b = ALPHABET.find(b) + 1
+
+print(f'Первая буква на {index_a} месте, а вторая на {index_b}.')
+print(f'Между ними {abs(index_a - index_b) - 1} букв.')
