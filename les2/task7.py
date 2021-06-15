dec31fe89add6d70ed97dd3e722df38617c1cb50
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-06-15 23:36:34 +0300

    1

--- les2/task7.py
+++ les2/task7.py
@@ -3,4 +3,19 @@
 что для множества натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
  где n - любое натуральное число.
 """
+import random as rnd
 
+
+def sum_(n):
+    if n == 1:
+        return n
+    return n + sum_(n - 1)
+
+
+test = [rnd.randint(1, 1000) for _ in range(100)]
+
+for n in test:
+    if sum_(n) == n * (n + 1) / 2:
+        print(f'Для {n=} равенство выполняется')
+    else:
+        print(f'Для {n=} равенство не выполняется')
--- les2/task9.py
+++ les2/task9.py
@@ -2,3 +2,20 @@
 Среди натуральных чисел, которые были введены, найти наибольшее по сумме цифр.
 Вывести на экран это число и сумму его цифр.
 """
+import random as rnd
+
+def sum_(n):
+    s = 0
+    for i in n:
+        s += int(i)
+    return s
+
+test = [rnd.randint(1, 1000000000) for _ in range(300)]
+max_ = 0
+for i in test:
+    s = sum_(str(i))
+    if max_ < s:
+        max_ = s
+
+print(f'Максимальная сумма цифр в числе = {max_}')
+
