cb66e993f149da9bcd9fb3b439f435b8a02c1d97
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-06-20 18:27:51 +0300

    3_1

--- les3/task4.py
+++ les3/task4.py
@@ -1,4 +1,13 @@
 """
 Определить, какое число в массиве встречается чаще всего.
 """
+import random as rnd
 
+ar = [rnd.randint(0, 100) for _ in range(100)]
+
+el = {i: 0 for i in set(ar)}
+
+for i in ar:
+    el[i] += 1
+
+print(max(el.values())) #переделать
