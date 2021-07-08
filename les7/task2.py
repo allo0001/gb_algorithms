635939000054a9bbfc76d004d69412804a0cadf3
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-07-08 23:35:21 +0300

    fin

--- les7/task2.py
+++ les7/task2.py
@@ -3,3 +3,49 @@
 заданный случайными числами на промежутке [0; 50).
 Выведите на экран исходный и отсортированный массивы.
 """
+import random as rnd
+
+
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+
+    return sorted_list
+
+
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+
+    mid = len(nums) // 2
+
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+
+    return merge(left_list, right_list)
+
+
+array = [rnd.randint(0, 50) for _ in range(20)]
+
+print(merge_sort(array))
